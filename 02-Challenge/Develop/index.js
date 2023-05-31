09852997f85d78ed5a7f10ebce9ec3ea1e528eff
Muramori71 <dannyroh2500@gmail.com>
2023-05-31 10:05:03 -0700

    added questions into the array

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the repository about?',
+},
+
+{
+    type: 'input',
+    name: 'table of contents',
+    message: 'What is in your table of contents?',
+},
+
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What is installed on your repository?',
+},
+{
+
+    
+}
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
